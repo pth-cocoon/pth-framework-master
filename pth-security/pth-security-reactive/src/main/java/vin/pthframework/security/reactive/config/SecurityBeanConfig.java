7f0285909440666622e49a2f587d223573dab5af
cocoon <6631097@qq.com>
2023-07-26 22:53:02 +0800

    重新处理错误码

--- pth-security/pth-security-reactive/src/main/java/vin/pthframework/security/reactive/config/SecurityBeanConfig.java
+++ pth-security/pth-security-reactive/src/main/java/vin/pthframework/security/reactive/config/SecurityBeanConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import reactor.core.publisher.Mono;
 import vin.pthframework.security.core.exception.BaseSecurityException;
@@ -29,19 +30,23 @@ public class SecurityBeanConfig {
   @ConditionalOnMissingBean(AuthorizationFailureHandler.class)
   @Bean
   public AuthorizationFailureHandler authorizationFailureHandler() {
-    return new AuthorizationFailureHandler() {
-      @Override
-      public Mono<Void> failureHandler(ServerHttpResponse response, BaseSecurityException e) {
-        response.setRawStatusCode(403);
-        DataBufferFactory bufferFactory = response.bufferFactory();
-        Map<String, Object> resultMap = ResultUtil.error(e);
-        DataBuffer wrap =
-            bufferFactory.wrap(toResultStr(resultMap).getBytes(StandardCharsets.UTF_8));
-        return response.writeWith(Mono.fromSupplier(() -> wrap));
+    return (response, e) -> {
+      String codeStr = (String.valueOf(e.getCode())).substring(0, 3);
+      switch (Integer.parseInt(codeStr)) {
+        case 401 -> response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        case 403 -> response.setStatusCode(HttpStatus.FORBIDDEN);
+        default -> {
+          log.error("未处理的异常", e);
+          response.setStatusCode(HttpStatus.FORBIDDEN);
+        }
       }
+      DataBufferFactory bufferFactory = response.bufferFactory();
+      Map<String, Object> resultMap = ResultUtil.error(e);
+      DataBuffer wrap =
+          bufferFactory.wrap(toResultStr(resultMap).getBytes(StandardCharsets.UTF_8));
+      return response.writeWith(Mono.fromSupplier(() -> wrap));
     };
   }
-
 
   private String toResultStr(Object o) {
     try {
