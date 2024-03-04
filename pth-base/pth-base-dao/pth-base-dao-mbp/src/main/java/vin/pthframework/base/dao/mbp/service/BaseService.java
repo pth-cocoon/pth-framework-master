d07925bad3e7f01a72336c76fd2d0b8f8b0eed4b
Cocoon <6631097@qq.com>
2024-03-04 16:55:07 +0800

    增加数据校验

--- pth-base/pth-base-dao/pth-base-dao-mbp/src/main/java/vin/pthframework/base/dao/mbp/service/BaseService.java
+++ pth-base/pth-base-dao/pth-base-dao-mbp/src/main/java/vin/pthframework/base/dao/mbp/service/BaseService.java
@@ -16,6 +16,18 @@ import vin.pthframework.base.dao.mbp.entity.BaseEntity;
 @SuppressWarnings("unused")
 public abstract class BaseService<T extends BaseEntity> {
 
+  protected void createCheck() {
+
+  }
+
+  protected void updateCheck() {
+
+  }
+
+  protected void deleteCheck() {
+
+  }
+
   @Getter
   @SuppressWarnings("unchecked")
   private final Class<T> modelClass =
@@ -26,8 +38,15 @@ public abstract class BaseService<T extends BaseEntity> {
     return Db.getById(id, getModelClass());
   }
 
+  /**
+   * 创建数据.
+   *
+   * @param model 创建.
+   * @return model.
+   */
   @SuppressWarnings("unused")
   public T create(T model) {
+    createCheck();
     Assert.notNull(model, "数据为空");
     Assert.isNull(model.getId(), "创建操作id必须为空");
     boolean result = Db.save(model);
@@ -35,22 +54,44 @@ public abstract class BaseService<T extends BaseEntity> {
     return getById(model.getId());
   }
 
-  public void createBatch(Collection<T> model) {
-    model.forEach(this::create);
-  }
 
+  /**
+   * 更新数据.
+   *
+   * @param model 根据ID update
+   * @return model.
+   */
   public T update(T model) {
+    updateCheck();
     Assert.notNull(model, "数据为空");
     Assert.notNull(model.getId(), "更新操作id不允许为空");
-    Assert.isTrue(model.getId()>0,"无法更新Id=0的数据");
+    Assert.isTrue(model.getId() > 0, "无法更新Id=0的数据");
     boolean result = Db.updateById(model);
     Assert.isTrue(result, "更新数据失败");
     return getById(model.getId());
   }
 
+  /**
+   * 根据ID删除
+   *
+   * @param id 主键
+   */
   public void deleteById(Integer id) {
+    deleteCheck();
     boolean result = Db.removeById(id, getModelClass());
     Assert.isTrue(result, "删除数据失败");
+  }
+
+  public void batchCreate(Collection<T> model) {
+    model.forEach(this::create);
+  }
+
+  public void batchUpdate(Collection<T> models) {
+    models.forEach(this::update);
+  }
+
+  public void batchDelete(Collection<Integer> id) {
+    id.forEach(this::deleteById);
   }
 
   public List<T> findAll() {
